b07efe8f83a7132e0ce062c10f90a4d6291d869e
ubuntupunk <ubuntupunk@gmail.com>
2020-09-21 11:34:54 +0200

    third commit

--- functions.php
+++ functions.php
@@ -848,6 +848,31 @@ add_filter( 'pt-ocdi/plugin_page_setup', 'pepe_lite_plugin_page_setup' );
 // No Banner 
 add_filter( 'pt-ocdi/disable_pt_branding', '__return_true' );
 
+//Some tweaks to login
+function my_login_logo() { ?>
+    <style type="text/css">
+        #login h1 a, .login h1 a {
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png);
+		height:65px;
+		width:320px;
+		background-size: 320px 65px;
+		background-repeat: no-repeat;
+        	padding-bottom: 30px;
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
+
+function my_login_logo_url() {
+    return home_url();
+}
+add_filter( 'login_headerurl', 'my_login_logo_url' );
+
+function my_login_logo_url_title() {
+    return 'Your Site Name and Info';
+}
+add_filter( 'login_headertitle', 'my_login_logo_url_title' );
+
 
 
 
